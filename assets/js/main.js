366fbca1da7261672fd7463e49ee33349a35b543
Joed Machado <joedmg58@gmail.com>
2024-01-23 17:00:59 -0500

    Added matrix effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,20 @@ let mouse = {
     radius: null
 }
 
-let x = document.getElementsByClassName('x');
-let y = document.getElementsByClassName('y');
+// let x = document.getElementsByClassName('x');
+// let y = document.getElementsByClassName('y');
 
-const myCanvas = document.getElementById('my-canvas');
-const ctx = myCanvas.getContext('2d');
+const canvas1 = document.getElementById('canvas1');
+const ctx = canvas1.getContext('2d');
+
+const canvas2 = document.getElementById('canvas2');
+const ctx2 = canvas2.getContext('2d');
+
 setCanvasSize();
 
 function setCanvasSize() {
-    CANVAS_WIDTH = myCanvas.width = window.innerWidth * 0.75;
-    CANVAS_HEIGHT = myCanvas.height = window.innerHeight * 0.75;
+    CANVAS_WIDTH = canvas2.width = canvas1.width = window.innerWidth * 0.45;
+    CANVAS_HEIGHT = canvas2.height = canvas1.height = window.innerHeight * 0.75;
     mouse.radius = CANVAS_HEIGHT/80 * CANVAS_WIDTH/80;
 }
 
@@ -25,34 +29,36 @@ window.onresize = function() {
     setCanvasSize();
 }   
 
-window.onmousemove = function(e) {
-    mouse.x = e.x - (window.innerWidth - CANVAS_WIDTH)/2; 
-    mouse.y = e.y - (window.innerHeight - CANVAS_HEIGHT)/2;
-    
-    ctx.beginPath();
-    ctx.fillStyle = 'red';
-    ctx.arc(mouse.x, mouse.y, 3, 0, 2*Math.PI);
-    ctx.closePath();
-    ctx.fill();
+// window.onmousemove = function(e) {
+//     const canvas1Rect = canvas1.getBoundingClientRect(); console.log(canvas1Rect);
 
-    ctx.beginPath();
-    ctx.strokeStyle = 'rgba(100, 100, 100, 0.18)';
-    ctx.arc(mouse.x, mouse.y, mouse.radius, 0, 2*Math.PI);
-    ctx.closePath();
-    ctx.stroke();
-}
+//     mouse.x = e.x - (window.innerWidth - CANVAS_WIDTH)/2; 
+//     mouse.y = e.y - (window.innerHeight - CANVAS_HEIGHT)/2;
+    
+//     ctx.beginPath();
+//     ctx.fillStyle = 'red';
+//     ctx.arc(mouse.x, mouse.y, 3, 0, 2*Math.PI);
+//     ctx.closePath();
+//     ctx.fill();
+
+//     ctx.beginPath();
+//     ctx.strokeStyle = 'rgba(100, 100, 100, 0.18)';
+//     ctx.arc(mouse.x, mouse.y, mouse.radius, 0, 2*Math.PI);
+//     ctx.closePath();
+//     ctx.stroke();
+// }
 
 const color = new randomColor();
 const position = new randomPosition({min: 0, max: CANVAS_WIDTH}, {min: 0, max: CANVAS_HEIGHT})
-const shapeTypes = ['circle', 'rectangle', 'triangle', 'star', 'penthagon', 'hexagon', 'octagon', '10'];
-const shapesCount = 500;
+const shapeTypes = ['circle'];//, 'rectangle', 'triangle', 'star', 'penthagon', 'hexagon', 'octagon', '10'];
+const shapesCount = CANVAS_HEIGHT * CANVAS_WIDTH / 9000;
 
 let shapes = [];
 
 for (let i = 0; i < shapesCount; i++) {
     let shapeOption = randomPickFromArray(shapeTypes); 
     switch (shapeOption) {
-        case 'circle': shapes.push(new Circle(position.getPosition(), randomFromRange(0, 2*Math.PI), randomFromRange(2, 5), color.getColor(), randomFromRange(3, 25)));
+        case 'circle': shapes.push(new Circle(position.getPosition(), randomFromRange(0, 2*Math.PI), randomFromRange(2, 5), color.getColor(), randomFromRange(5, 10)));
                        break;
         case 'rectangle': let a = randomFromRange(3,25);
                           shapes.push(new Rectangle(position.getPosition(), randomFromRange(0, 2*Math.PI), randomFromRange(2, 5), color.getColor(), a, a));
@@ -73,9 +79,12 @@ for (let i = 0; i < shapesCount; i++) {
 }
 
 function drawShapes(ctx) {
+    connect();
+    
     for (let i=0; i<shapes.length; i++) {
         shapes[i].draw(ctx);
     }
+    
 }
 
 function updateShapes(ctx) {
@@ -96,6 +105,27 @@ function checkCollisions(boundX, boundY, mouse) {
         shapes[i].checkCollision(boundX, boundY, mouse);
     }
 }
+
+function connect() {
+    let opacity = 1;
+    for (let i=0; i<shapes.length; i++) {
+        for (j=i; j<shapes.length; j++) {
+            let distance = (shapes[i].position.x - shapes[j].position.x)*(shapes[i].position.x - shapes[j].position.x) + (shapes[i].position.y - shapes[j].position.y)*(shapes[i].position.y - shapes[j].position.y);
+            if (distance < (CANVAS_WIDTH/7)*(CANVAS_HEIGHT/7)) {
+                opacity = 1 - (distance/20000);
+                ctx.strokeStyle = `rgba(208,230,19,${opacity})`; //`rgba(140,85,31,${opacity})`;
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(shapes[i].position.x, shapes[i].position.y);
+                ctx.lineTo(shapes[j].position.x, shapes[j].position.y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+const effect = new Effect(canvas2.width, canvas2.height);
+
 
 var fps = 30;
 var now;
@@ -118,6 +148,12 @@ function animate() {
         drawShapes(ctx);
         updateShapes();
         checkCollisions({min: 0, max: CANVAS_WIDTH}, {min: 0, max:CANVAS_HEIGHT}, mouse);
+
+
+        ctx2.fillStyle = `rgba(0, 0, 0, 0.05)`;
+        ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
+        ctx2.font = effect.fontSize + 'px monospace';
+        effect.symbols.forEach(symbol => symbol.draw(ctx2));
     }
 
 }
---
+++ assets/js/matrix.js
@@ -0,0 +1,39 @@
+class Symbol {
+    constructor(x, y, fontSize, canvasHeight) {
+        this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = '';
+        this.canvasHeight = canvasHeight;
+    }
+
+    draw(ctx) {
+        this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
+        ctx.fillStyle = '#0aff0a';
+        ctx.fillText(this.text, this.x*this.fontSize, this.y*this.fontSize);
+        if (this.y * this.fontSize > this.canvasHeight) {
+            this.y = 0;
+        } else {
+            this.y += 1;
+        }
+    }
+}
+
+class Effect {
+    constructor(canvasWidth, canvasHeight) {
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.fontSize = 25;
+        this.columns = this.canvasWidth / this.fontSize;
+        this.symbols = [];
+
+        this.#initialize();
+    }
+
+    #initialize() {
+        for (let i=0; i<this.columns; i++) {
+            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+        }
+    }
+}
